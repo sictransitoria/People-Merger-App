1df58b6838b0f198690a108efbb38fb808b6d5a7
Michael Oria <Michael_Oria@me.com>
2018-04-18 10:34:27 -0400

    Updated with working model.

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 // After creating two .JSON files with data;
 
 const fs = require("fs");
-const groupOne = require("./peopleOne.json");
-const groupTwo = require("./peopleTwo.json");
 const completeFile = "peopleComplete.txt";
 
 
 // Function that will read both .JSON files and write them into one .txt file;
 
 function combineTheTwo(groupOne, groupTwo) {
+var groupOne;
+var groupTwo;
+var combination;
+var combination2;
+var combination3;
 
 fs.readFile(groupOne, "utf-8", function(err, dataOne) {
   if(err) throw err;
-  var groupOne = JSON.parse(dataOne);
+  groupOne2 = JSON.parse(dataOne);
+
 
 fs.readFile(groupTwo, "utf-8", function(err, dataTwo) {
   if(err) throw err;
-  var groupTwo = JSON.parse(dataTwo);
-  var combination = groupOne.concat(groupTwo);
-    console.log(combination.sort());
+  groupTwo2 = JSON.parse(dataTwo);
+  combination = groupOne2.concat(groupTwo2);
+  combination2 = combination.sort();
+  console.log(combination2);
 
-fs.writeFile("./peopleComplete.txt", combination.sort(), "utf-8", function(err) {
-  if(err) 
-  throw err;
+  combination3 = JSON.stringify(combination2);
 
-});
+fs.writeFile(completeFile, combination3, (err)=>{
+  console.log("peopleComplete.txt file created")
+    
+    })
+  
+  })
+
+})
+
+}
 
 // Call function to combine both .JSON files into one text file;
 
-combineTheTwo("./people1.json", "./people2.json");
-
-});
+combineTheTwo("./peopleOne.json", "./peopleTwo.json");
